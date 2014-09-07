f2b90a7e8d731b264a2b8c39c67f32a815d20f5b
godbod <gkounkou@gmail.com>
2014-09-07 16:40:48 +0200

    Updated QmlManager for login

--- includes/QmlManager.h
+++ includes/QmlManager.h
@@ -10,21 +10,32 @@
 
 #include <QtDeclarative>
 #include "MessageModelsps.h"
+#include "DataBase.h"
 
-class QmlManager
+class QmlManager : public QObject
 {
+    Q_OBJECT
+
 private:
     QDeclarativeView* configureQMLInterface(MessageModelSPS *);
+    QDeclarativeView* configureLogin();
 
 public:
     QmlManager();
     ~QmlManager();
     void showQMLInterface(MessageModelSPS *);
+    void startLogin();
+
+public slots:
+    void slotStartChat();
+    void slotStopLogin();
 
 private:
     int height;
     int width;
     QString qmlPath;
+
+    QDeclarativeView *viewLogin;
 };
 
 #endif // QMLMANAGER_H
