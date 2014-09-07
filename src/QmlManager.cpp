fbf1ba47b7b0c9d990d5724720898b51dbf5d7f9
godbod <gkounkou@gmail.com>
2014-09-07 16:42:30 +0200

    updated QmlManager

--- src/QmlManager.cpp
+++ src/QmlManager.cpp
@@ -4,8 +4,26 @@ QmlManager::QmlManager()
 {
     this->height  = 550;
     this->width   = 300;
-    this->qmlPath = "../resources/main.qml";
-    //this->qmlPath = "../resources/login.qml";
+    this->qmlPath = "";
+}
+
+void QmlManager::slotStartChat()
+{
+    DataBase *dbLoad = new DataBase();
+    MessageModelSPS *modelsps;
+    modelsps = dbLoad->LoadDataBaseSPS(modelsps);
+    this->showQMLInterface(modelsps);
+}
+
+void QmlManager::startLogin()
+{
+    viewLogin = configureLogin();
+    viewLogin->show();
+}
+
+void QmlManager::slotStopLogin()
+{
+    viewLogin->close();
 }
 
 void QmlManager::showQMLInterface(MessageModelSPS *modelsps)
@@ -14,12 +32,28 @@ void QmlManager::showQMLInterface(MessageModelSPS *modelsps)
     view->show();
 }
 
-QDeclarativeView* QmlManager::configureQMLInterface(MessageModelSPS *mdsps)
+QDeclarativeView* QmlManager::configureLogin()
 {
     QDeclarativeView *view = new QDeclarativeView();
+    view->setSource(QUrl::fromLocalFile("../resources/login.qml"));
+    QObject *item;
+    item = view->rootObject();
+    view->setResizeMode(QDeclarativeView::SizeRootObjectToView);
+    view->setMinimumSize(this->width, this->height);
+    view->setMaximumSize(this->width + 200, this->height - 200);
+    
+    QObject::connect((QObject *)item, SIGNAL(quitLogin()),  this, SLOT(slotStartChat()));
+    QObject::connect((QObject *)item, SIGNAL(quitLogin()),  this, SLOT(slotStopLogin()));
+
+    return view;
+}
+
+QDeclarativeView* QmlManager::configureQMLInterface(MessageModelSPS *mdsps)
+{
+    QDeclarativeView    *view = new QDeclarativeView();
     QDeclarativeContext *ctxt = view->rootContext();
     ctxt->setContextProperty("myModelsps", mdsps);
-    view->setSource(QUrl::fromLocalFile(qmlPath));
+    view->setSource(QUrl::fromLocalFile("../resources/main.qml"));
     QObject *item;
     item = view->rootObject();
     view->setResizeMode(QDeclarativeView::SizeRootObjectToView);
