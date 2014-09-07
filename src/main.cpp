cbf83fe395e266b6b2d5ae9000b2f4c47fad1163
godbod <gkounkou@gmail.com>
2014-09-07 16:43:18 +0200

    updated main to launch login at first

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
 
-
-
 #include <QApplication>
-#include "MessageModelsps.h"
 #include "QmlManager.h"
-#include "DataBase.h"
 
 int main (int argc, char **argv)
 {   
     QApplication app(argc, argv);
 
-    DataBase *dbLoad = new DataBase();
-    MessageModelSPS *modelsps;
-    modelsps   = dbLoad->LoadDataBaseSPS(modelsps);
     QmlManager *qmlMan = new QmlManager();
-    qmlMan->showQMLInterface(modelsps);
+    qmlMan->startLogin();
 
     return app.exec();
 }
