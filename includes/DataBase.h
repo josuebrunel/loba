1d783c670703d1e465306cea45bfe1f669a4c4c4
godbod <gkounkou@gmail.com>
2014-09-08 23:07:46 +0200

    added Database header file

--- includes/DataBase.h
+++ includes/DataBase.h
@@ -1,21 +1,33 @@
-/**
- * author : godbod
- * file   : Database.h
- * date   : 2014-08-25
- * brief  : the database header file
- */
+//  Copyright (c) 2014 godbod
+//
+//////////////////////////////////////////
+///
+/// Author : G
+/// File   : DataBase.h
+/// Date   : 2014 Aug 25
+/// Brief  : DataBase class file
+///
+//////////////////////////////////////////
 
 #ifndef DATABASE_H
 #define DATABASE_H
 
-#include "MessageModelsps.h"
+#include "MessageModel.h"
+
+/// DataBase class
 
 class DataBase
 {
 public:
+
+    /// constructor
     DataBase();
 
-    MessageModelSPS* LoadDataBaseSPS(MessageModelSPS *);
+    /// destructor
+    ~DataBase();
+
+    /// DataBase loader
+    MessageModel* LoadDataBase(MessageModel *);
 };
 
 #endif // DATABASE_H
