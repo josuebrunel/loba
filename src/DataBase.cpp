4630bffbdbb3c7688a8a49c904c1caad4837d7c3
godbod <gkounkou@gmail.com>
2014-09-08 23:14:49 +0200

    added comment

--- src/DataBase.cpp
+++ src/DataBase.cpp
@@ -1,14 +1,29 @@
+//  Copyright (c) 2014 godbod
+//
+//////////////////////////////////////////
+///
+/// Author : G
+/// File   : DataBase.h
+/// Date   : 2014 Aug 25
+/// Brief  : DataBase implementation file
+///
+//////////////////////////////////////////
+
 #include "DataBase.h"
 
+///------------------
 DataBase::DataBase()
+///------------------
 {
 }
 
-MessageModelSPS* DataBase::LoadDataBaseSPS(MessageModelSPS *modelsps)
+///--------------------------------------------------------
+MessageModel* DataBase::LoadDataBase(MessageModel *model)
+///--------------------------------------------------------
 {
-    modelsps = new MessageModelSPS();
+    /// get the model
+    model = new MessageModel();
+    model->addMessage(Message("Welcome on loba"));
 
-    modelsps->addMessage(Message("Welcome on loba"));
-
-    return modelsps;
+    return model;
 }
