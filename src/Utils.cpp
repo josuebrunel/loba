65ec940e60349031100d92e5e145dafd06a6c4df
gkounkou <gkounkou@gmail.com>
2014-11-23 04:23:09 +0100

    added ../src/

---
+++ src/Utils.cpp
@@ -0,0 +1,60 @@
+//  Copyright (c) 2014 godbod
+//
+//////////////////////////////////////////
+///
+/// Author : G
+/// File   : Utils.cpp
+/// Date   : 2014 Aug 25
+/// Brief  : Utils implementation file
+///
+//////////////////////////////////////////
+
+
+#include "Utils.h"
+#include <iostream>
+
+using namespace std;
+
+///-----------------------------------
+Utils::Utils()
+///-----------------------------------
+{
+   /// nothing to delete
+}
+
+///-----------------------------------
+Utils::~Utils()
+///-----------------------------------
+{
+}
+
+///-----------------------------------
+QTime Utils::getCurrentTime() const
+///-----------------------------------
+{ 
+   return QTime::currentTime();
+}
+
+///----------------------------------------------------------------------------
+QString Utils::processMessage(QString message)
+///----------------------------------------------------------------------------
+{
+    bool flag = false;
+
+    /// cout << message.toStdString() << endl;
+    QString processedMessage = "";
+
+    for (int i = 0; i < message.size(); i++) {
+       if ((message.at(i) == QChar('!')) && (flag == false)) {
+          flag = true;
+          processedMessage.append(message.at(i));
+          processedMessage.append(" : ");
+       } if (message.at(i) == QChar('#')) {
+          flag = false;
+       } else if (flag == false) {
+          processedMessage.append(message.at(i));
+       }
+    }
+
+    return processedMessage;
+}
